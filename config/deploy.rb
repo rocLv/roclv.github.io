0d2540a1fd4fc2e4704ee72ea786fa6abbbec4d4
rocLv <wangqsh999@gmail.com>
2015-12-19 10:40:07 +0800

    modify after task

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,9 +43,7 @@ namespace :deploy do
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
-      within release_path do
-        execute :jekyll, 'b'
-      end
+      execute :jekyll, 'b'
     end
   end
 
