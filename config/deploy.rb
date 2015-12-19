17e9aac9c3f5deaff6eccfa7d795f709d9f358ca
rocLv <wangqsh999@gmail.com>
2015-12-19 10:52:57 +0800

    modify task

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :deploy_to, '/var/www/china-ruby'
 namespace :deploy do
 
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    on roles(:all), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
